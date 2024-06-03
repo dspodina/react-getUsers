f955029d400c8c2a13cdc2629240f04d04b19796
dspodina <spodina.darya@gmail.com>
2024-06-03 16:35:22 +0200

    style edited

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,13 +1,22 @@
 import PropTypes from "prop-types";
+import { FaPhoneVolume } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
+import { CgWebsite } from "react-icons/cg";
 import "./User.css";
 
 const User = ({ user }) => {
   return (
     <li className="user-card">
       <h2>{user.name}</h2>
-      <p className="email">{user.email}</p>
-      <p className="phone">{user.phone}</p>
-      <p className="website">{user.website}</p>
+      <p className="contact-item email">
+        <MdOutlineMail className="icon" /> {user.email}
+      </p>
+      <p className="contact-item phone">
+        <FaPhoneVolume className="icon" /> {user.phone}
+      </p>
+      <p className="contact-item website">
+        <CgWebsite className="icon" /> {user.website}
+      </p>
     </li>
   );
 };
@@ -18,7 +27,7 @@ User.propTypes = {
     email: PropTypes.string.isRequired,
     phone: PropTypes.string.isRequired,
     website: PropTypes.string.isRequired,
-  }),
+  }).isRequired,
 };
 
 export default User;
